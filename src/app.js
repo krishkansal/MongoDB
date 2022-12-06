1a76e6d729082c3a7d24809e68df141e813af44e
krishkansal <krishkansal7@gmail.com>
2022-12-06 21:00:02 +0530

    data saved

--- src/app.js
+++ src/app.js
@@ -1,69 +1,29 @@
-const mongoose = require("mongoose");
-const validator=require("validator");
+const express = require('express');
+require("./Db/conn");
+const Student=require("./models/students")
+const app=express();
+const port = 8000|| process.env.PORT;
 
 
-// mongoose -> Prod Dep --> Help ypu connect to your cloud DB ( .connect(connectionString) )
-mongoose.connect("mongodb://localhost:27017/demo")
-//mongodb://localhost:27017/demo
-
-.then( () => console.log("connection seccessfull..."))
-.catch(err=>console.log(err));
-
-//Mongoose Schema
-const playlistSchema=new mongoose.Schema({
-   
-    name:mongoose.Schema.Types.String,
-    ctype:mongoose.Schema.Types.String,
-    video:{
-        type:Number,
-        validate(value){
-            if(value<0){
-                throw new Error("video count should be greater then 0")
-            }
-        }
-    },
-    email:{
-        type:String,
-        validate(value){
-            if(!validator.isEmail(value)){
-                throw new Error("Email is invalid");
-            }
-        }
-
-    }
-})
-
-
-//collection creation
-const Playlist = new mongoose.model("Playlist",playlistSchema)
-
-
-//documet
-const NodePlaylist = new Playlist({
+app.use(express.json());
+app.get('/',(req,res)=>{
     
-    name:"Node JS",
-    ctype:"Back End",
-    //video:"javascript"
+    res.send("hello from home side");
 
 })
 
-const reactPlaylist = new Playlist({
+app.post("/students",(req,res)=>{
+    console.log(req.body);
     
-    name:"JS",
-    ctype:"Front End",
-    video:1,
-    email:"krishkansal7@gmail.com"
+    const user = new Student(req.body);
+    
+    user.save()
+    .then(()=>{ res.status(201).send(user);})
+    .catch((e) => {res.status(400).send(e);})
 
+     
 })
 
-//document save
-Playlist.insertMany([reactPlaylist,NodePlaylist])
-.then( () => console.log("seccessfull Done.."))
-.catch(err=>console.log(err));
-
-
-const getDocument=async()=>{
-    const result=await Playlist.find({ctype:"Front End"}).select({name:1});
-    console.log(result);
-}
-getDocument();
+app.listen(port,()=>{
+    console.log('connection setup');
+})
